7213124d6cde62e3eb66c903d9ac4483527cfcf2
Jose Moreno <jgmorenor277@gmail.com>
2023-02-19 19:56:49 -0500

    Se creó archive.php, para organizar las categorías de las entradas

--- front-page.php
+++ front-page.php
@@ -18,7 +18,7 @@
             <?php
             $args = array(
                 'post_type' => 'producto',
-                'post_per_page' => -1,
+                'posts_per_page' => -1,
                 'order' => 'ASC',
                 'orderby' => 'title'
 
